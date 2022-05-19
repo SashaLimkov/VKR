227d4ff139d6d19b231e68e4a6d34c117e304269
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-05-20 02:04:28 +0300

    refactor + get_calendars

--- src/bot/services/google_calendar/create_and_delete_calendar.py
+++ src/bot/services/google_calendar/create_and_delete_calendar.py
@@ -1,16 +1,10 @@
 from pprint import pprint
-from Google import create_service
-
-CLIENT_SECRET_FIlE = "client_secret.json"
-API_NAME = "calendar"
-API_VERSION = "v3"
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-
-service = create_service(CLIENT_SECRET_FIlE, API_NAME, API_VERSION, SCOPES)
+from log_in import connect_to_calendar
 
 
-def create_calendar(worker, calendar_name: str):
+def create_calendar(calendar_name: str):
     # need to add response to database
+    worker = connect_to_calendar()
     request_body = {
         "summary": f"{calendar_name}"
     }
@@ -18,25 +12,31 @@ def create_calendar(worker, calendar_name: str):
     return response
 
 
-def delete_calendar(worker, calendar_id: str):
-    print(1)
+def delete_calendar(calendar_id: str):
+    worker = connect_to_calendar()
     return worker.calendars().delete(calendarId=calendar_id).execute()
 
 
-"""
-Get calendars list
-"""
-# ids_ = []
-# page_token = None
-# while True:
-#     calendar_list = service.calendarList().list(pageToken=page_token).execute()
-#     for calendar_list_entry in calendar_list["items"]:
-#         ids_.append(calendar_list_entry["id"])
-#     page_token = calendar_list.get("nextPageToken")
-#     if not page_token:
-#         break
-# for calendar_id in ids_:
-#     print(calendar_id)
-# if __name__ == '__main__':
+def get_calendars():
+    worker = connect_to_calendar()
+    current_calendars = {}
+    page_token = None
+    while True:
+        calendar_list = worker.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list["items"]:
+            current_calendars.update(
+                {
+                    "calendar_name": calendar_list_entry["summary"],
+                    "calendar_id": calendar_list_entry["id"]
+                }
+            )
+        page_token = calendar_list.get("nextPageToken")
+        if not page_token:
+            break
+    return current_calendars
+
+
+if __name__ == '__main__':
     # pprint(create_calendar(worker=service, calendar_name="Хирург"))
+    pprint(get_calendars())
     # pprint(delete_calendar(worker=service, calendar_id="me0ea2ia1po7t83vmscm09jek8@group.calendar.google.com"))
