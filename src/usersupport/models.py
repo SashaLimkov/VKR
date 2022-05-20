fb0c7822b6ec0fd6a494838de7b2364493b5a27a
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-05-20 09:36:52 +0300

    try to add telegram_user and userQuestion models

--- src/usersupport/models.py
+++ src/usersupport/models.py
@@ -14,14 +14,27 @@ class TimeBasedModel(models.Model):
 
 class TelegramUser(TimeBasedModel):
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = "Телеграм Юзер"
+        verbose_name_plural = "Телеграм Юзеры"
 
     id = models.AutoField(primary_key=True)
     user_id = models.BigIntegerField(unique=True, verbose_name="UserID")
     name = models.CharField(max_length=255, verbose_name="UserName")
     user_role = models.CharField(max_length=255, verbose_name="Роль", default="пользователь")
-    state = models.IntegerField(verbose_name="Работает?", default=1)
     phone = models.CharField(max_length=12, unique=True)
-    chat_id = models.BigIntegerField(verbose_name="Чат пользователя", default=0)
+    email = models.CharField(max_length=255, unique=True)
+    chanel_chat_id = models.BigIntegerField(verbose_name="Чат пользователя", default=0)
     chanel_id = models.BigIntegerField(verbose_name="Канал пользователя", default=0)
+
+
+class UserQuestion(TimeBasedModel):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        TelegramUser, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    question = models.CharField(max_length=5000, verbose_name="Анкета")
+    state = models.CharField(
+        max_length=100, verbose_name="Состояние", default="Открытый заявка"
+    )
+    mes_id = models.CharField(max_length=20000, unique=False, default="")
+
