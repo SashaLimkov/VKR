d4747da595a7d883d1e8b9a0580b4a013af557d4
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-05-24 07:59:00 +0300

    fixes

--- src/bot/utils/cleaner.py
+++ src/bot/utils/cleaner.py
@@ -12,14 +12,17 @@ async def delete_command_message(message: types.Message):
 
 
 async def delete_messages(user_id):
-    message_id_list = user_data[user_id]["messages_to_delete"]
-    print(message_id_list)
-    for mes_id in message_id_list:
-        try:
-            await bot.delete_message(
-                chat_id=user_id,
-                message_id=mes_id
-            )
-        except Exception as e:
-            print(e)
-    user_data[user_id]["messages_to_delete"] = []
+    try:
+        message_id_list = user_data[user_id]["messages_to_delete"]
+        print(message_id_list)
+        for mes_id in message_id_list:
+            try:
+                await bot.delete_message(
+                    chat_id=user_id,
+                    message_id=mes_id
+                )
+            except Exception as e:
+                print(e)
+        user_data[user_id]["messages_to_delete"] = []
+    except Exception as e:
+        print(e)
--- src/bot/utils/user_menu.py
+++ src/bot/utils/user_menu.py
@@ -1,5 +1,25 @@
+from bot.config.loader import bot, user_data
+from bot.services.db.doctor import select_doctor
 from usersupport.models import TelegramUser
+from bot.data import text_data as td
+from bot.keyboards import inline as ik
 
 
 async def send_user_menu(user: TelegramUser):
-    pass
+    doctor = await select_doctor(user=user)
+    if doctor:
+        sent_message = await bot.send_photo(
+            chat_id=user.user_id,
+            caption=td.DOCTOR_PROFILE.format(
+                name=user.name,
+                education=user_data[user.user_id]["education"],
+                experience=user_data[user.user_id]["experience"],
+                profession=user_data[user.user_id]["profession"],
+            ),
+            photo=user_data[user.user_id]["photo"],
+            reply_markup=await ik.doctor_pp()
+        )
+        user_data[user.user_id]["messages_to_delete"].append(sent_message.message_id)
+    else:
+        sent_message = await bot.send_message(chat_id=user.user_id, text=td.GET_READY)
+        user_data[user.user_id]["messages_to_delete"].append(sent_message.message_id)
